37535082d3808c57ee0a8804552e7aeaf68e49ad
sigurdur (Mr ) <sigurdur@hafro.is>
2014-01-17 09:04:36 +0000

    mrgrid fixed, now similar to other grid-funs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geo@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mrgrid.R
+++ pkg/R/mrgrid.R
@@ -1,13 +1,20 @@
 mrgrid <-
-function (mr, dlat = 5, dlon = 10, fill = FALSE, ...) 
+function(mr, dlat = 5, dlon = 10, fill = FALSE, ...)
 {
-  mr <- lapply(mr, mrPeri, dlat = dlat, dlon = dlon)
-  if(fill) {
-    invisible(lapply(1:length(mr), function(i, mr, ...)
-      geopolygon(mr[[i]], col = col[i], ...), mr = mr, ...))
-  }
-  else {
-    invisible(lapply(1:length(mr), function(i, mr, ...)
-      geolines(mr[[i]], col = col[i], ...), mr = mr, ...))
-  }
+  n <- length(mr)
+  lat <- mr2d(mr, dlat = dlat, dlon = dlon)$lat
+  lon <- mr2d(mr, dlat = dlat, dlon = dlon)$lon
+  lat <- c(rep(lat, 5), rep(NA, n))
+  lon <- c(rep(lon, 5), rep(NA, n))
+  lat <- as.vector(matrix(matrix(lat, nrow = 6, byrow = T), ncol = 1))
+  lon <- as.vector(matrix(matrix(lon, nrow = 6, byrow = T), ncol = 1))
+  lat <- lat + c(dlat/120, dlat/120,  - dlat/120,  - dlat/120, dlat/
+  	120, NA)
+  lon <- lon + c(dlon/120,  - dlon/120,  - dlon/120, dlon/120, dlon/
+  	120, NA)
+  lat <- data.frame(lat = lat, lon = lon)
+  if(fill)
+  	geopolygon(lat, ...)
+  else geolines(lat, ...)
 }
+
