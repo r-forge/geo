fde0e9a7ec1d26dcd72be1b6d46b33740af3fe58
sigurdur (Mr ) <sigurdur@hafro.is>
2014-04-08 14:22:41 +0000

    added ir2d, the inverse of d2ir, with some documentation. Switched encoding in two declarations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geo@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/ir2d.R
@@ -0,0 +1,20 @@
+ir2d <-
+function(ir, useI = FALSE)
+{
+  lat <- substring(ir, 1, 2)
+  lat <- as.numeric(lat)
+  lat <- (lat +71)/2 + 0.25
+  lon1 <- substring(ir, 3, 3)
+  lon1 <- toupper(lon1)
+  lon1 <- match(lon1, LETTERS)
+  if(!useI)
+    lon1 <- ifelse(lon1 > 8, lon1 - 1, lon1)
+  lon1 <- lon1 - 2
+  lon2 <- substring(ir, 4)
+  lon2 <- as.numeric(lon2)
+  if(lon1 < 0) 
+    lon <- -44 + lon2 + 0.5
+  else 
+    lon <- -40 + 10*lon1 + lon2 + 0.5
+  data.frame(lat = lat, lon = lon)
+}
